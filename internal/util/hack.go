fa1e1c0c29e1f1f4cf748d458afda0f61b370b4c
Denys Smirnov <dennwc@pm.me>
2022-03-06 17:31:10 +0200

    implement atomics

--- internal/util/hack.go
+++ internal/util/hack.go
@@ -1,1 +1,17 @@
 package util
+
+import "sync/atomic"
+
+type VpxAtomicInt struct {
+	Value int64
+}
+
+func VpxAtomicInit(p *VpxAtomicInt, value int) {
+	p.Value = int64(value)
+}
+func VpxAtomicStoreRelease(p *VpxAtomicInt, value int) {
+	atomic.StoreInt64(&p.Value, int64(value))
+}
+func AtomicLoadAcquire(p *VpxAtomicInt) int {
+	return int(atomic.LoadInt64(&p.Value))
+}
